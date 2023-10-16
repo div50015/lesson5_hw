e957907bc5d37ce1b9924fb3f2ccecbbe1c244c1
Igor Degtyarenko <div50015@mail.ru>
2023-10-16 14:25:08 +0300

    final commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,15 @@ from selenium import webdriver
 def browser_management():
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.config.timeout = 2.0
-    browser.config.window_width = 1200
+    browser.config.window_width = 1800
     browser.config.window_height = 1080
 
 #    browser.config.type_by_js = True
-
-#    driver_options = webdriver.ChromeOptions()
+    # закрытие сообщения о том что браузер запущен в отладочном режиме
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_experimental_option("excludeSwitches", ['enable-automation'])
 #    driver_options.add_argument('--headless=new')
-#    browser.config.driver_options = driver_options
+    browser.config.driver_options = driver_options
 
     yield
 
--- tests/test_demoqa_operations.py
+++ tests/test_demoqa_operations.py
@@ -1,9 +1,15 @@
 import time
 from selene import browser, have
 import os
+from selene import command
 
 def test_complete_todo():
     browser.open('/')
+    # уменьшение размера изображения в 0.8 раза
+    browser.execute_script('document.querySelector(".body-height").style.transform = "scale(.8)"')
+    # ожидание в течение 10 секунд 3х реклпмных банеров и их удаление (вторая строка)
+    # browser.all('[id^google_ads][id$=container__]').with_(timeout=10).wait_until(have.size_less_than_or_equal(3))
+    # browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
 
     browser.element('#firstName').type('Ivan')
     browser.element('#lastName').type('Petrov')
@@ -21,15 +27,15 @@ def test_complete_todo():
     browser.element('#react-select-3-input').type('NCR').press_enter()
     browser.element('#react-select-4-input').type('Noida').press_enter()
     browser.element('#submit').click()
-    #time.sleep(5)
-    browser.element('.table').all('tr td:nth-child(2)').should(have.texts(
-    'Ivan Petrov',
-    'petrov@mail.ru',
-    'Male',
-    '7928777777',
-    '09 August,1999',
-    'Biology',
-    'Sports',
-    'ball.jpg',
-    'Russia Moscow',
-    'NCR Noida'))
+    time.sleep(10)
+    browser.element('.table').all('tr td:nth-child(2)').should(have.exact_texts(
+        'Ivan Petrov',
+        'petrov@mail.ru',
+        'Male',
+        '7928777777',
+        '09 August,1999',
+        'Biology',
+        'Sports',
+        'ball.jpg',
+        'Russia Moscow',
+        'NCR Noida'))
